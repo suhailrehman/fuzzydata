375c8af29bde10bb087e8134259077e74c8e0951
Suhail Rehman <suhailrehman@gmail.com>
2022-03-03 23:11:20 -0600

    Release 0.0.1 to pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="fuzzydata-test",
+    name="fuzzydata",
     version="0.0.1",
     scripts=['./scripts/fuzzydata'],
     author="Suhail Rehman",
