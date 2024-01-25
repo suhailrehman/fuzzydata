ad793c99e4fd4296239ee81c4cf1ce2463f70406
Suhail Rehman <suhailrehman@users.noreply.github.com>
2024-01-25 13:29:59 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="fuzzydata",
-    version="0.0.8",
+    version="0.0.9",
     scripts=['./scripts/fuzzydata'],
     author="Suhail Rehman",
     author_email="suhailrehman@gmail.com",
